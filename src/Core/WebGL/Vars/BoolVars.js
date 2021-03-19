5458cb73910daf1d8277d80cd439ce5f9e9df9a8
JhonKkk <1724624287@qq.com>
2021-03-19 22:04:58 +0800

    PrincipledLighting完善

---
+++ src/Core/WebGL/Vars/BoolVars.js
@@ -0,0 +1,30 @@
+import Vars from "./Vars.js";
+
+/**
+ * BoolVars。<br/>
+ * 布尔变量。<br/>
+ * @author Kkk
+ * @date 2021年3月19日21点44分
+ */
+export default class BoolVars extends Vars{
+    constructor(props) {
+        super(props);
+        this._m_Bool = false;
+    }
+    valueOf(bool){
+        this._m_Bool = bool;
+        return this;
+    }
+    /**
+     * 比较值。<br/>
+     * @param {BoolVars}[boolVars]
+     * @return {Boolean}
+     */
+    compare(boolVars) {
+        return this._m_Bool == boolVars._m_Bool;
+    }
+    _upload(gl, loc, fun){
+        gl.uniform1i(loc, this._m_Bool);
+    }
+
+}
